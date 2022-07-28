70eb3044e645ecf3239a277ae54a2078f5ad30e3
felipe <fortes.felipe95@gmail.com>
2022-07-28 17:03:41 -0300

    Requisito 02 - Criar o componente Wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    console.log(this.props);
+    const { userEmail } = this.props;
+    return (
+      <header>
+        <div data-testid="email-field">
+          Olá,
+          {' '}
+          { userEmail }
+        </div>
+        <div data-testid="total-field">
+          Despesa total: 0
+        </div>
+
+        <div data-testid="header-currency-field">
+          BRL
+        </div>
+
+      </header>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (globalState) => ({
+  userEmail: globalState.user.email,
+});
+
+Wallet.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
+
+/*
+ESTADO GLOBAL
+
+{
+  user: {
+    email: '',
+  }
+}
+*/
