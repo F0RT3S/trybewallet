df24c6eb2b418beb2822dbd09db198ff6317a4a2
felipe <fortes.felipe95@gmail.com>
2022-07-28 16:35:26 -0300

    Requisito 01 - Configuração do Redux e implementação dos campos de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,84 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import saveUserEmail from '../redux/actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleClick = () => {
+    const { history, setUserEmail } = this.props;
+    const { email } = this.state;
+    setUserEmail(email);
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Hm Bifinhuuuu</div>;
+    const SEIS = 6;
+    const { email, password } = this.state;
+    return (
+      <form>
+        <label htmlFor="name">
+          Email:
+          <input
+            data-testid="email-input"
+            type="email"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+        </label>
+
+        <label htmlFor="password">
+          Senha:
+          <input
+            data-testid="password-input"
+            type="password"
+            name="password"
+            value={ password }
+            onChange={ this.handleChange }
+            placeholder="*******"
+          />
+        </label>
+
+        <button
+          type="button"
+          disabled={
+            password.length < SEIS || !email.includes('@') || !email.includes('.com')
+          }
+          onClick={ this.handleClick }
+        >
+
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  setUserEmail: (email) => dispatch(saveUserEmail(email)),
+});
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  setUserEmail: PropTypes.string.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,7 @@
 // Coloque aqui suas actions
+const saveUserEmail = (email) => ({
+  type: 'SET_USER_EMAIL',
+  payload: email,
+});
+
+export default saveUserEmail;
