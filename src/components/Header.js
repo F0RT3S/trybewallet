5c8f0b3fd28cbead943a75352db23fb90559ab8a
felipe <fortes.felipe95@gmail.com>
2022-08-02 14:22:19 -0300

    Requisito 04 - Depois de muita luta, muito stress e raiva, 4/4

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,8 @@ import WalletForm from './WalletForm';
 
 class Header extends React.Component {
   render() {
-    const { userEmail } = this.props;
+    const { userEmail, totalValue } = this.props;
+    console.log(totalValue);
     return (
       <header>
         <div data-testid="email-field">
@@ -14,7 +15,10 @@ class Header extends React.Component {
           { userEmail }
         </div>
         <div data-testid="total-field">
-          Despesa total: 0
+          {
+            totalValue.reduce((acc, currency) => (acc + parseFloat(currency.value)
+              * parseFloat(currency.exchangeRates[currency.currency].ask)), 0).toFixed(2)
+          }
         </div>
 
         <div data-testid="header-currency-field">
@@ -30,10 +34,12 @@ class Header extends React.Component {
 
 const mapStateToProps = (globalState) => ({
   userEmail: globalState.user.email,
+  totalValue: globalState.wallet.expenses,
 });
 
 Header.propTypes = {
   userEmail: PropTypes.string.isRequired,
+  totalValue: PropTypes.arrayOf(Object).isRequired,
 };
 
 export default connect(mapStateToProps, null)(Header);
